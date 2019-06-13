6717ff79259af0f071588cad5e6c8b8b6acd7e49
tech-camp <>
2019-06-13 17:37:54 +0900

    セレクトボックス　スポット名　作成

---
+++ resources/views/reviews/index.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="/assets/stylesheets/style.css">
+    <title>レビュー一覧</title>
+  </head>
+  <body>
+    <header>
+      <h1>レビュー一覧</h1>
+    </header>
+    <div class="box1">
+      スポット名
+    </div>
+      <div class="cp_ipselect cp_sl01">
+<select required>
+  <option value="" hidden>Choose</option>
+  <option value="1">中国</option>
+  <option value="2">韓国</option>
+  <option value="3">台湾</option>
+  <option value="4">香港</option>
+</select>
+</div>
+    </div>
+    <div class="contents">
+      <div class="message clearfix">
+        <div class="message_box">
+          <p class="user_name">
+            アダム
+          </p>
+          <p class="text">
+            富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山
+            富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山
+            富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山・富士山
+          </p>
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+          <p class="user_name">たま
+          </p>
+          <p class="text">
+            鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川・鴨川
+          </p>
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+        </div>
+      </div>
+      <div class="message clearfix">
+        <div class="message_box">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
