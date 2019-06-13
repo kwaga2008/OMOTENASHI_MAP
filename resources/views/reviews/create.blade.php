89986939c200c1f36c4da59c52f3f08b0dab7e7b
tech-camp <>
2019-06-13 14:44:56 +0900

    投稿画面の生成

---
+++ resources/views/reviews/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layout')
+
+@section('content')
+<body td align="center">
+
+<h1>Kinkaku-ji</h1>
+
+<p>
+<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+<p>Nick name
+<input type="text" name="personal_name"></p>
+<p>
+<input type="radio" name="q1" value="Good"> Good
+<input type="radio" name="q1" value="Bad"> Bad
+<input type="radio" name="q1" value="Omotenashi"> Omotenashi
+</p>
+<p>Country<br>
+<select name="country">
+<option value="country1">Japan</option>
+<option value="country2">Korea</option>
+<option value="country3">Chine</option>
+<option value="country4">Taiwan</option>
+<option value="country5">Hongkong</option>
+<option value="country6">Thailand</option>
+<option value="country7">America</option>
+<option value="country8">Singapore</option>
+<option value="country9">malaysia</option>
+<option value="country10">Indonesia</option>
+<option value="country11">Philippine</option>
+<option value="country12">Vietnam</option>
+<option value="country13">India</option>
+<option value="country14">England</option>
+<option value="country15">France</option>
+<option value="country16">Germany</option>
+</select>
+</p>
+<p>Review</p>
+<textarea name="contents" rows="8" cols="100">
+</textarea><br><br>
+<input type="submit" name="btn3" value="send">
+</form>
+</td>
+</body>
+@endsection
