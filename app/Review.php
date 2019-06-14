3faa26b919133fc6154e84c0f1081da40e53e2cd
tech-camp <>
2019-06-14 12:35:12 +0900

    miss

--- app/Review.php
+++ app/Review.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     public function place(){
-      return $this->belongsTo(Review;;class);
+      return $this->belongsTo(Place::class);
     }
-    
+
     protected $fillable = array('nickname', 'text', 'country', 'feeling');
 }
