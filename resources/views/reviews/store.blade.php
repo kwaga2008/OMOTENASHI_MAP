f856995aa7e5916f2c6a3ccc87272fbca8bf7044
tech-camp <>
2019-06-14 12:27:22 +0900

    投稿画面完成（仮）

--- resources/views/reviews/store.blade.php
+++ resources/views/reviews/store.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('content')
-
+    <div class="contents row">
+        <div class="success">
+            <h3>
+                投稿が完了しました。
+            </h3>
+            <a class="btn" href="/reviews">投稿一覧へ戻る</a>
+        </div>
+    </div>
 @endsection
