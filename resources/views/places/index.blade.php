68e3eee94d55efe1e04ac508205d4e2836d64fd5
tech-camp <>
2019-06-14 16:41:30 +0900

    トップページ（仮完成）

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -18,14 +18,29 @@
 </head>
 <body>
 <div class="slider">
-<img src="images/toppage/Fuji.jpg" width="0" height="0" alt="">
+<img src="images/toppage/fuji.jpg" width="0" height="0" alt="">
 <img src="images/toppage/kyoto.jpg" width="0" height="0" alt="">
 <img src="images/toppage/tokyo.jpg" width="0" height="0" alt="">
-
 </div>
-<h1>Top page</h1>
-<a href="/areas">名所を絞り込むとこ</a>
-<a href="/areas/show">その他の観光地を探す</a>
+
+<div td align="center">
+<h1>Select Area</h1>
+<div class="area_selects">
+<ul>
+<li><img src="images/toppage/tokyo_menu.jpg" width="300" height="300" alt="" align=""><p>Tokyo</p></li>
+<li><a href="/areas"><img src="images/toppage/kyoto_menu.jpg" width="300" height="300" alt=""><p>Kyoto</a></li>
+<li><img src="images/toppage/osaka_menu.jpg" width="300" height="300" alt=""><p>Osaka</p></li>
+<br><br>
+<li><img src="images/toppage/hokkaido_menu.jpg" width="300" height="300" alt=""><p>Hokkaido</p></li>
+<li><img src="images/toppage/okinawa_menu.jpg" width="300" height="300" alt=""><p>Okinawa</p></li>
+<li><img src="images/toppage/nagoya_menu.png" width="300" height="300" alt=""><p>Nagoya</p></li>
+</ul>
+</div>
+<br><br>
+<a href="/areas/show">The others</a>
+<br>
+<br>
+</div>
 </body>
 
 @endsection
