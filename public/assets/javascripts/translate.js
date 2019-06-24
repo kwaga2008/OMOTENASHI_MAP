1f9f754caae3caec63ae9f1c7c0bb0c9335a2c05
kwaga2008 <>
2019-06-24 11:41:11 +0900

    translate

--- public/assets/javascripts/translate.js
+++ public/assets/javascripts/translate.js
@@ -1,6 +1,6 @@
 function translate(str,translate_into) {
     AWS.config.region = 'ap-northeast-1'; // Region
-    AWS.config.credentials = new AWS.Credentials("AKIA53LPTATPYIEBZOML", "3wom/07H//P1Z+JRxGpUKD+pvtEBAARtUA+mWPhd");
+    AWS.config.credentials = new AWS.Credentials();
     var translate = new AWS.Translate({
         region: AWS.config.region,
         credentials: AWS.config.credentials,
@@ -13,36 +13,54 @@ function translate(str,translate_into) {
       };
       translate.translateText(params, function(err, data) {
           if (err) {
-              console.log(err, err.stack); // an error occurred
-              return "error";
-        } else {
-              console.log(data["TranslatedText"]); 
-              
-            return data["TranslatedText"];
-        }           // successful response
+                console.log(err, err.stack); // an error occurred
+                return "error";  
+          } else {
+                console.log(data["TranslatedText"]); 
+                return data["TranslatedText"];         
+        }          
       });
         
 }
+
+
 $('#translate').on('click', function () {
     var translate_into = $('#translate_option').val();
     console.log(translate_into);
-var request = $.ajax({
-    type: 'GET',
-    url: "/areas/places/infos",
-    cache: false,
-    dataType: "json",
-    data: {
-        "place_id": place_id,
-    },
-    timeout: 3000
-});
+    var request = $.ajax({
+        type: 'GET',
+        url: "/areas/places/infos",
+        cache: false,
+        dataType: "json",
+        data: {
+            "place_id": place_id,
+        },
+        timeout: 3000
+    });
 
-/* 成功時 */
-request.done(function (data) {
-    $('#infobox').empty();
-    if (Object.keys(data).length != 0) {
-        var s = "<p>"+translate(data["information"],translate_into)+"</p>";
+    /* 成功時 */
+    request.done(function (data) {
+        $('#infobox').empty();
+        if (Object.keys(data).length != 0) {
+            var s1;
+            var promise1 = new Promise(function (resolve, reject){
+                s1 = translate(data["information"], translate_into);  
+                console.log("s1:"+s1);
+                setTimeout(function () {
+                       resolve(s1);
+                }, 20000);
+                
+             });
+        
+        promise1.then(function (value) {
+            var s = '<p>'+ value +'</p>';
+            console.log(s);
             $('#infobox').append(s);
+        }).catch(function (error) {
+            // 非同期処理失敗。呼ばれない
+            console.log(error);
+        });
+           
     } else {
         console.log("failed");
         var content = 'Translate failed';
