5218753dba70e5f52e421d97e6ed86dc4c54ee39
kwaga2008 <>
2019-06-26 17:37:41 +0900

    no message

---
+++ database/seeds/AreasSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Area;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $file = new SplFileObject('database/seeds/areas.csv');
+        $file->setFlags(SplFileObject::READ_CSV);
+
+        $ary = array();
+        foreach($file as $data) {
+          array_push($ary, $data);
+        }
+
+        array_shift($ary);
+        foreach($ary as $data) {
+          Area::create(
+            array(
+              'area' => $data[1],
+              'area_longitude' => $data[2],
+              'area_latitude' => $data[3],
+              'created_at' => $data[4],
+              'updated_at' => $data[5],
+              'area_info' => $data[6],
+              
+            )
+          );
+        }
+    }
+}
