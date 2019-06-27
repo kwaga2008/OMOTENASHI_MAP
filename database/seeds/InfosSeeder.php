5be5bb8194c02385fa3dfc76f7ac33010aa59bd2
kwaga2008 <>
2019-06-27 09:58:00 +0900

    no message

--- database/seeds/InfosSeeder.php
+++ database/seeds/InfosSeeder.php
@@ -12,25 +12,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $file = new SplFileObject('database/seeds/infos.csv');
-        $file->setFlags(SplFileObject::READ_CSV);
+        // // $this->call(UsersTableSeeder::class);
+        // $file = new SplFileObject('database/seeds/infos.csv');
+        // $file->setFlags(SplFileObject::READ_CSV);
 
-        $ary = array();
-        foreach($file as $data) {
-          array_push($ary, $data);
-        }
+        // $ary = array();
+        // foreach($file as $data) {
+        //   array_push($ary, $data);
+        // }
 
-        array_shift($ary);
-        foreach($ary as $data) {
-          Info::create(
-            array(
-              'id' => $data[0],
-              'place_id' => $data[1],
-              'information' => $data[2],
+        // array_shift($ary);
+        // foreach($ary as $data) {
+        //   Info::create(
+        //     array(
+        //       'id' => $data[0],
+        //       'place_id' => $data[1],
+        //       'information' => $data[2],
               
-            )
-          );
-        }
+        //     )
+        //   );
+        // }
     }
 }
