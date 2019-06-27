318bd9d72911ad4f75740a1bc62c82563d3803ab
kwaga2008 <>
2019-06-27 09:55:40 +0900

    no message

--- database/seeds/ReviewsSeeder.php
+++ database/seeds/ReviewsSeeder.php
@@ -12,27 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $file = new SplFileObject('database/seeds/reviews.csv');
-        $file->setFlags(SplFileObject::READ_CSV);
+        // // $this->call(UsersTableSeeder::class);
+        // $file = new SplFileObject('database/seeds/reviews.csv');
+        // $file->setFlags(SplFileObject::READ_CSV);
 
-        $ary = array();
-        foreach($file as $data) {
-          array_push($ary, $data);
-        }
+        // $ary = array();
+        // foreach($file as $data) {
+        //   array_push($ary, $data);
+        // }
 
-        array_shift($ary);
-        foreach($ary as $data) {
-          Review::create(
-            array(
-              'id' => $data[0],
-              'nickname' => $data[1],
-              'text' => $data[2],
-              'country' => $data[3],
-              'feeling' => $data[4],
-              'place_id' => $data[5],
-            )
-          );
-        }
+        // array_shift($ary);
+        // foreach($ary as $data) {
+        //   Review::create(
+        //     array(
+        //       'id' => $data[0],
+        //       'nickname' => $data[1],
+        //       'text' => $data[2],
+        //       'country' => $data[3],
+        //       'feeling' => $data[4],
+        //       'place_id' => $data[5],
+        //     )
+        //   );
+        // }
     }
 }
